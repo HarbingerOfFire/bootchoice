834dc6e7ae6a15e165c4fc1175d0b4c78303a554
HarbingerOfFire <HarbingerOfFire@proton.me>
2024-10-15 16:08:24 -0600

    shutdown on exit

--- bootchoice.c
+++ bootchoice.c
@@ -26,28 +26,30 @@ int main() {
         scanf("%d", &buf);
 
         switch (buf) {
-        case 0:
-            // Launch Command Prompt (CLI Mode)
-            system("conhost.exe"); //old graphics terminal, automatically opens to cmd.exe
-            break;
-        case 1:
-            // Kill any existing explorer processes
-            system("taskkill /F /IM explorer.exe 2> C:/NUL"); //kill lingering proccess of explorer.exe (output stored in null if errors)
+            case 0:
+                // Launch Command Prompt (CLI Mode)
+                system("conhost.exe"); //old graphics terminal, automatically opens to cmd.exe
+                break;
+            case 1:
+                // Kill any existing explorer processes
+                system("taskkill /F /IM explorer.exe 2> C:/NUL"); //kill lingering proccess of explorer.exe (output stored in null if errors)
 
-            // Execute PowerShell command to start explorer.exe as TrustedInstaller
-            system("explorer.exe");
-            break;
-        case 2:
-            //run something else via windows run dialog (use shell32 so you don't have to launch full gui)
-            system("rundll32 shell32.dll,#61");
-        case 3:
-            // Exit the program
-            printf("Exiting shell...\n");
-            return 0;
-        default:
-            // Handle invalid input
-            printf("Incorrect Choice. Please try again.\n");
-            continue; // Go back to the start of the loop
+                // Execute PowerShell command to start explorer.exe as TrustedInstaller
+                system("explorer.exe");
+                break;
+            case 2:
+                //run something else via windows run dialog (use shell32 so you don't have to launch full gui)
+                system("rundll32 shell32.dll,#61");
+                break;
+            case 3:
+                // Exit the program
+                printf("Exiting shell...\n");
+                system("shutdown /s /t 0");
+                break;
+            default:
+                // Handle invalid input
+                printf("Incorrect Choice. Please try again.\n");\
+                continue; // Go back to the start of the loop
         }
 
         break; // Exit the loop if a valid option was chosen
